c2fcdd0443ec227f8331573f09923cebd5019ed8
Vincen <vincenhayate123@gmail.com>
2024-10-15 10:28:15 +0700

    Fix user icon when mobile view

--- resources/views/component/navigation/user-dropdown.blade.php
+++ resources/views/component/navigation/user-dropdown.blade.php
@@ -1,4 +1,4 @@
-<img id="avatarButton" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-end" class="hidden lg:block w-10 h-10 rounded-full cursor-pointer" aria-hidden="true" src="/docs/images/people/profile-picture-5.jpg" alt="User dropdown">
+<img id="avatarButton" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-end" class="block w-10 h-10 rounded-full cursor-pointer" aria-hidden="true" src="/docs/images/people/profile-picture-5.jpg" alt="User dropdown">
 <div id="userDropdown" class="hidden z-10 bg-white divide-y divide-gray-200 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
     <div class="py-1">
         <a href="#" class="flex gap-2 items-center justify-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
